6e76e5c43ebb287a8445ae75eab6a25b2bcf3faf
unknown <muhammadrafinuriman@student.uns.ac.id>
2022-05-27 18:23:10 +0700

    Mengerjakan Exercise 11

---
+++ Exercise-11/Data/Geometri2D.py
@@ -0,0 +1,35 @@
+# Silakan perhatikan perintah pada file 11_Modul&Paket.ipynb sebelum membuka file ini
+
+import math
+
+# Fungsi luas persegi panjang
+def LuasPersegiPanjang (p,l):
+    return p*l
+
+# Fungsi keliling persegi panjang
+def KelilingPersegiPanjang (p,l):
+    return 2*(p+l)
+
+# Fungsi luas bujur sangkar
+def LuasBujurSangkar (s):
+    return s*s
+
+# Fungsi keliling bujur sangkar
+def KelilingBujurSangkar (s):
+    return 4*s
+
+# Fungsi luas lingkaran
+def LuasLingkaran (r):
+    return math.pi*r*r
+
+# Fungsi keliling lingkaran
+def KelilingLingkaran (r):
+    return 2*math.pi*r 
+
+# Fungsi luas segitiga
+def LuasSegitiga (a,t):
+    return (a*t)/2
+
+# Fungsi keliling segitiga
+def KelilingSegitiga (a,b,c):
+    return a+b+c
